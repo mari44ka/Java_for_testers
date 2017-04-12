5f615bc78e9948c5be7d3e23789960715fe6b53f
mari44ka <marikostenko83@gmail.com>
2017-04-12 00:38:37 -0700

    определение расстояния между точками
    Создан класс Point;
    Создана функция
    public static double distance(Point p1, Point p2);
    Сделан запускаемый класс, то есть содержащий функцию
    public static void main(String[] args) {…};

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -10,6 +10,24 @@ public class MyFirstProgram {
     System.out.println("Площадь прямоугольника со сторонами " + r.a +" и " + r.b +" = " + r.area());
 
     System.out.println("Hello, world");
+
+
+    Point p1 = new Point();
+    p1.x=0;
+    p1.y=0;
+
+    Point p2 = new Point();
+    p2.x=5;
+    p2.y=0;
+
+    System.out.println("Расстояние между двумя точками " + "("+(p1.x+p1.y)+")" +" и "+"("+(p2.x+p2.y)+")"+" = "+distance(p1,p2));
+
+  }
+
+
+
+  public static double distance (Point p1,Point p2){
+    return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
   }
 
   }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Mari on 4/11/17.
+ */
+public class Point {
+  public double x;
+  public double y;
+}
