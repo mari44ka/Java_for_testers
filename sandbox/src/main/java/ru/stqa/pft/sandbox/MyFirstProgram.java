8fa36220527725ddf6f0ec2affb30757f5766333
mari44ka <marikostenko83@gmail.com>
2017-04-12 22:58:54 -0700

    расстояние между точками при помощи метода в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -20,17 +20,17 @@ public class MyFirstProgram {
     //p2.x=5;
     //p2.y=0;
 
-    System.out.println("Расстояние между двумя точками " + "("+(p1.x+p1.y)+")" +" и "+"("+(p2.x+p2.y)+")"+" = "+distance(p1,p2));
+    System.out.println("Расстояние между двумя точками " + "("+p1.x+","+p1.y+")" + " и " + "("+p2.x +","+ p2.y+")"+" = "+ p1.distance(p2));
 
   }
 
 
 
-  public static double distance (Point p1,Point p2){
-    return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+  //public static double distance (Point p1,Point p2){
+    //return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
   }
 
-  }
+
 
 
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -12,4 +12,12 @@ public class Point {
     this.y=y;
 
   }
+  double distance (double x,double y){
+
+    return Math.sqrt((this.x-x)*(this.x-x)+(this.y-y)*(this.y-y));
+  }
+
+  double distance(Point p1){
+    return distance(p1.x,p1.y);
+  }
 }
