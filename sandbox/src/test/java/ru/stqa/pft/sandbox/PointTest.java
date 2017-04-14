7c2fd9c0abff0eb43fd0097ab12d5c5fed943fb5
mari44ka <marikostenko83@gmail.com>
2017-04-14 11:28:45 -0700

    добавлены тесты в класс PointTest

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -6,12 +6,27 @@ import org.testng.annotations.Test;
 /**
  * Created by Mari on 4/13/17.
  */
+
 public class PointTest {
 
   @Test
   public void testDistance(){
     Point p1 = new Point(0,0);
     Point p2 = new Point(5,0);
+
     Assert.assertEquals(p1.distance(p2),5.0);
   }
+  @Test
+  public void testDistance1(){
+    Point p1 = new Point(-3,-8);
+    Point p2 = new Point(3,2);
+
+    Assert.assertEquals(p1.distance(p2),11.661903789690601);
+  }
 }
+
+
+
+
+
+
