dd78a342e3972fef35d0e351f19bdf3d15c07bfe
mari44ka <marikostenko83@gmail.com>
2017-04-14 14:42:07 -0700

    перенос логина в часть инициализации фикстуры. Удаление лишних записей рекордером

--- adressbook_web_tests/src/test/java/ru/stqa/pft/adressbook/GroupCreationtests.java
+++ adressbook_web_tests/src/test/java/ru/stqa/pft/adressbook/GroupCreationtests.java
@@ -21,15 +21,8 @@ public class GroupCreationtests {
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreationtests() {
+
         wd.get("http://localhost/addressbook/index.php");
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
@@ -37,6 +30,11 @@ public class GroupCreationtests {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+    
+    @Test
+    public void testGroupCreation() {
+
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
