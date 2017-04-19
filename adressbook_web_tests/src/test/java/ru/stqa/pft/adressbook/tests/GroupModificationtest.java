648735d241664b31d652a44df2f84c2fee1b45dd
mari44ka <marikostenko83@gmail.com>
2017-04-18 23:56:38 -0700

    GroupModification test created without recoder

---
+++ adressbook_web_tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationtest.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.adressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.adressbook.model.GroupData;
+
+/**
+ * Created by Mari on 4/18/17.
+ */
+public class GroupModificationtest extends TestBase{
+
+
+
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHelper().Gotogrouppage();
+    app.getGroupHelper().selectgroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillForm(new GroupData("Test2", "test3", " "));
+    app.getGroupHelper().submitGroupModification();
+    app.getNavigationHelper().Gotogrouppage();
+
+  }
+
+}
