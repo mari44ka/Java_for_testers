b6caa7c8d38396287926d3c9f7dae6a0358d1038
mari44ka <marikostenko83@gmail.com>
2017-05-10 14:15:28 -0700

    addded new assertion check with lambda

--- adressbook_web_tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationtests.java
+++ adressbook_web_tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationtests.java
@@ -8,29 +8,30 @@ import ru.stqa.pft.adressbook.model.GroupData;
 import java.util.HashSet;
 import java.util.List;
 
-public class GroupCreationtests extends TestBase{
+public class GroupCreationtests extends TestBase {
 
 
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().Gotogrouppage();
-    List<GroupData>before=app.getGroupHelper().getGroupList(); // переменная before поменялось,
+    List<GroupData> before = app.getGroupHelper().getGroupList(); // переменная before поменялось,
     // теперь она содержит не количество элементов, а список
     //int before=app.getGroupHelper().getGroupCount(); // узнаем количество групп до добавления
-    GroupData group = new GroupData("Test1", "test2", "test3");
+    GroupData group = new GroupData("Test4", "test2", "test3");
     app.getGroupHelper().createGroup(group);
     app.getNavigationHelper().Gotogrouppage();
-    List<GroupData>after = app.getGroupHelper().getGroupList();
+    List<GroupData> after = app.getGroupHelper().getGroupList();
     //int after = app.getGroupHelper().getGroupCount(); // узнаем количество групп после добавления
-    Assert.assertEquals(after.size(),before.size()+1);// проверка, что группа добавилась;
+    Assert.assertEquals(after.size(), before.size() + 1);// проверка, что группа добавилась;
     // когда меняем переменную after, меняется на after.size()
 
-    int max =0;
-    for (GroupData g: after){
-      if (g.getId()>max){
-        max=g.getId();
-      }
-    }
+    //int max =0;
+    //for (GroupData g: after){   //находим максимальный id
+    //if (g.getId()>max){
+    //max=g.getId();
+
+    //Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(),o2.getId());
+    int max =after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId();
     group.setId(max);
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
@@ -40,3 +41,4 @@ public class GroupCreationtests extends TestBase{
 
 }
 
+// we replaced comparartor with lamda
