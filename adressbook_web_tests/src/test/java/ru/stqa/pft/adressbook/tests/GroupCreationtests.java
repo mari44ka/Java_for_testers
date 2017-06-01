f589d4268119e4c0bc7d85311a54a959cd41c5ad
mari44ka <marikostenko83@gmail.com>
2017-06-01 15:41:31 -0700

    creating groups with different group data using dataProvider

--- adressbook_web_tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationtests.java
+++ adressbook_web_tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationtests.java
@@ -3,14 +3,13 @@ package ru.stqa.pft.adressbook.tests;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import ru.stqa.pft.adressbook.model.GroupData;
 import ru.stqa.pft.adressbook.model.Groups;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -18,15 +17,23 @@ import static org.testng.Assert.assertEquals;
 
 public class GroupCreationtests extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validGroups(){
+    List<Object[]>list = new ArrayList<Object[]>();
+    list.add(new Object[]{new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+    list.add(new Object[]{new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+    list.add(new Object[]{new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+    return list.iterator();
+  }
 
-  @Test
-  public void testGroupCreation() {
+
+  @Test(dataProvider ="validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().grouppage();
     Groups before = app.group().all(); //теперь работаем с множеством, а не списками
     //List<GroupData> before = app.getGroupHelper().list(); // переменная before поменялось,
     // теперь она содержит не количество элементов, а список
     //int before=app.getGroupHelper().сount(); // узнаем количество групп до добавления
-    GroupData group = new GroupData().withName("Test4").withHeader("test2").withFooter("test3");
     app.group().create(group);
     app.goTo().grouppage();
     assertThat(app.group().сount(), equalTo(before.size() + 1));
